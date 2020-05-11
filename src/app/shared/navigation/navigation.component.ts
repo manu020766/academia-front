3f5eda0428770ebcadd40e620a60f0b92b831c82
Manu <manu020766@yahoo.es>
2020-05-12 00:59:21 +0200

    navigation diferent between mobile and desktop

--- src/app/shared/navigation/navigation.component.ts
+++ src/app/shared/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core'
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  // Declare height and width variables
+  // scrHeight:any
+  scrWidth:any
+  open:boolean
+  style:string
 
-  ngOnInit(): void {
+  @HostListener('window:resize', ['$event'])
+  getScreenSize(event?) {
+        // this.scrHeight = window.innerHeight
+        this.scrWidth = window.innerWidth
+
+        if (this.scrWidth > 1024) {
+          this.open = true
+          this.style = 'side'
+        } else {
+          this.open = false
+          this.style = 'push'
+        } 
   }
+
+  constructor() {
+    this.getScreenSize()
+   }
+
+  ngOnInit(): void {}
 
 }
